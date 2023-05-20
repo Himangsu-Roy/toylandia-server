335963fff79b8b44a728f044e60eaefe1c0895ec
himangsu roy <himur98@gmail.com>
2023-05-20 11:57:45 +0600

    necessery middleware added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const cors = require("cors")
 const port = process.env.PORT || 5000;
 const app = express();
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get("/", (req, res) => {
     res.send("ToyLandia Server is Running")
