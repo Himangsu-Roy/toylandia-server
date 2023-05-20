5b45a3699e6ad015177eb7f8900b3a6984d87b68
himangsu roy <himur98@gmail.com>
2023-05-20 12:45:56 +0600

    added env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
 
 // middleware
 app.use(cors());
@@ -12,7 +13,9 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://<username>:<password>@cluster0.ys20m5d.mongodb.net/?retryWrites=true&w=majority";
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ys20m5d.mongodb.net/?retryWrites=true&w=majority`;
+  
+    console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
