967a99069b9473fca3ba8768dd3ed88413dd1959
himangsu roy <himur98@gmail.com>
2023-05-20 13:31:42 +0600

    added post add a toy and get all toys

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ys20m5d.mongodb.net/?retryWrites=true&w=majority`;
   
-    console.log(uri)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +29,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      
+      const toyCollection = client.db("toyLandia").collection("toy");
+
+
+      app.get("/alltoys", async (req, res) => {
+          const toys = await toyCollection.find().toArray();
+          res.send(toys)
+      });
+
+      app.post("/addatoy", async (req, res) => {
+          const toy = req.body;
+          console.log(toy)
+          const result = await toyCollection.insertOne(toy);
+          res.send(result)
+
+      });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
