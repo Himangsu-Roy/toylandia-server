96a410f7b72bb1ae92129f7fc0d369689449d5c2
himangsu roy <himur98@gmail.com>
2023-05-21 23:48:05 +0600

    finishing commit

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ys20m5d.mongodb.net/?retryWrites=true&w=majority`;
@@ -35,8 +33,8 @@ async function run() {
 
 
       app.get("/alltoys", async (req, res) => {
-          const toys = await toyCollection.find().limit(20).toArray();
-          res.send(toys)
+          const toys = await toyCollection.find().limit(20).sort({ price: 1 }).toArray();
+        res.send(toys);
       });
 
       app.get("/toydetails/:id", async (req, res) => {
@@ -57,7 +55,6 @@ async function run() {
 
       app.post("/addatoy", async (req, res) => {
           const toy = req.body;
-          console.log(toy)
           const result = await toyCollection.insertOne(toy);
           res.send(result)
 
